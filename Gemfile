07f0b878d969b60f287c44b10e8cf914eea062c8
hogehoge <hogehoge@example.com>
2021-07-02 14:12:23 +0900

    recommit

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
 group :development do
@@ -48,6 +51,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-commands-rspec'
+  gem 'launchy'
 end
 
 group :test do
@@ -56,6 +61,10 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'webdrivers'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,47 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+  def new
+    @blog = Blog.new
+  end
+  def create
+    @blog = Blog.new(blog_params)
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "タスクを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+  def show
+    @blog = Blog.find(params[:id])   # 重複！
+  end
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
+  end
+  def edit
+    @blog = Blog.find(params[:id])   # 重複！
+  end
+  def update
+    @blog = Blog.find(params[:id])   # 重複！
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @blog = Blog.find(params[:id])  
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+end
